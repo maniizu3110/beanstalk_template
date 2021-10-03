14b6c213de7eda0ddbca4e80109f00b8e85810a0
maniizu3110 <maniizu3110@gmail.com>
2021-10-03 15:29:31 +0900

    create

---
+++ client/pages/users/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-btn @click="getUsers" color="primary" elevation="2" x-large x-small
+      >Get Users</v-btn
+    >
+    <v-data-table
+      :headers="headers"
+      :items="users"
+      :items-per-page="10"
+      class="elevation-1"
+    ></v-data-table>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: "Name",
+          align: "start",
+          sortable: false,
+          value: "name",
+        },
+        { text: "Email", value: "email" },
+      ],
+      users: [],
+    };
+  },
+  mounted() {
+    this.getUsers();
+  },
+  methods: {
+    getUsers() {
+      this.$axios
+        .get("api/v1/user/")
+        .then((res) => {
+          this.users = res.data;
+        })
+    },
+  },
+};
+</script>
